1fe6671d85af1032308efedb75e8cbeb5349743d
Mickael Vieira <contact@mickael-vieira.com>
2015-03-23 10:38:46 +0000

    Added q parameter to request

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var express = require('express');
+var qString = require('querystring');
 var hbs     = require('hbs');
 var http    = require('http');
 var app     = express();
-
-var querystring = require('querystring');
 
 app.use(express.static('public'));
 app.set('view engine', 'html');
@@ -17,35 +16,35 @@ app.get('/', function (req, res) {
 
 app.get('/search', function (req, res) {
 
+    var queryString = qString.stringify({
+        'q' : req.query.q
+    });
+
     var options = {
         hostname: 'siapi.dev',
         post: 80,
-        path: '/search',
+        path: '/search?' + queryString,
         method: 'GET',
         headers: {
             'Accept': 'application/vnd.collection+json'
         }
     };
 
-    var postData = querystring.stringify({});
     var request = http.request(options, function (response) {
 
         console.log('STATUS: ' + response.statusCode);
         console.log('HEADERS: ' + JSON.stringify(response.headers));
 
         response.setEncoding('utf8');
-
         response.on('data', function (body) {
             res.send(body);
         });
-
     });
 
     request.on('error', function(e) {
         console.log('problem with request: ' + e.message);
     });
 
-    request.write(postData);
     request.end();
 });
 
